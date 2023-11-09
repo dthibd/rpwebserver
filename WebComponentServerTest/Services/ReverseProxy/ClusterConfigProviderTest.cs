3a53f23d78fdf67084dd16c9aa1bb08bfbef3273
Danny T <cobreti_@hotmail.com>
2023-11-08 20:05:41 -0500

    completed ClustersConfigProvider tests

--- WebComponentServerTest/Services/ReverseProxy/ClusterConfigProviderTest.cs
+++ WebComponentServerTest/Services/ReverseProxy/ClusterConfigProviderTest.cs
@@ -20,8 +20,8 @@ public class ClusterConfigProviderTest
                 ClusterId = "clusterC"
             });
 
-        provider.Clusters.Add("clusterA", new MutableClusterConfig("clusterA"));
-        provider.Clusters.Add("clusterB", new MutableClusterConfig("clusterB"));
+        provider.CreateCluster("clusterA");
+        provider.CreateCluster("clusterB");
         provider.Clusters.Add("clusterC", clusterConfigMock.Object);
 
         var clusters = provider.ToClusterConfigList();
@@ -45,8 +45,112 @@ public class ClusterConfigProviderTest
     public void CreateClusterWithExistingId()
     {
         var provider = new ClustersConfigProvider();
-        provider.Clusters.Add("test", new MutableClusterConfig("test"));
+        provider.CreateCluster("test");
 
         Assert.Throws<ArgumentException>(() => provider.CreateCluster("test"));
+    }
+
+    [Fact]
+    public void ListClustersIds()
+    {
+        var provider = new ClustersConfigProvider();
+        provider.CreateCluster("testA");
+        provider.CreateCluster("testB");
+
+        var ids = provider.ListClusterIds();
+
+        Assert.NotNull(ids);
+        Assert.Equal(2, ids.Count);
+        Assert.Contains("testA", ids);
+        Assert.Contains("testB", ids);
+    }
+
+    [Fact]
+    public void GetClusterByIdWithValidId()
+    {
+        var provider = new ClustersConfigProvider();
+        provider.CreateCluster("testA");
+        provider.CreateCluster("testB");
+
+        var cluster = provider.GetClusterById("testA");
+
+        Assert.NotNull(cluster);
+        Assert.Equal("testA", cluster.Id);
+    }
+
+    [Fact]
+    public void GetClusterByIdWithInvalidId()
+    {
+        var provider = new ClustersConfigProvider();
+        var cluster = provider.GetClusterById("test");
+
+        Assert.Null(cluster);
+    }
+
+    [Fact]
+    public void ListClustersWithTwoElements()
+    {
+        var provider = new ClustersConfigProvider();
+        provider.CreateCluster("testA");
+        provider.CreateCluster("testB");
+
+        var clusters = provider.ListClusters();
+
+        Assert.NotNull(clusters);
+        Assert.Equal(2, clusters.Count);
+    }
+
+    [Fact]
+    public void AddCluster()
+    {
+        var provider = new ClustersConfigProvider();
+        provider.AddCluster(new MutableClusterConfig("test"));
+
+        Assert.Single(provider.Clusters);
+        Assert.Equal("test", provider.Clusters.First().Key);
+    }
+
+    [Fact]
+    public void AddClusterWithExistingId()
+    {
+        var provider = new ClustersConfigProvider();
+        provider.CreateCluster("test");
+
+        Assert.Throws<ArgumentException>(() => provider.AddCluster(new MutableClusterConfig("test")));
+    }
+
+    [Fact]
+    public void UpdateCluster()
+    {
+        var provider = new ClustersConfigProvider();
+        provider.CreateCluster("test");
+
+        Assert.Null(provider.Clusters.First().Value.LoadBalancingPolicy);
+
+        var newCluster = new MutableClusterConfig("test")
+        {
+            LoadBalancingPolicy = "LBP-Test"
+        };
+
+        provider.UpdateCluster(newCluster);
+
+        var updatedCluster = provider.Clusters.First().Value;
+
+        Assert.NotNull(updatedCluster);
+        Assert.NotNull(updatedCluster.LoadBalancingPolicy);
+        Assert.Equal("LBP-Test", updatedCluster.LoadBalancingPolicy);
+    }
+
+    [Fact]
+    public void UpdateClusterWithInvalidId()
+    {
+        var provider = new ClustersConfigProvider();
+        
+        var newCluster = new MutableClusterConfig("test")
+        {
+            LoadBalancingPolicy = "LBP-Test"
+        };
+
+        Assert.Throws<ArgumentException>(() => provider.UpdateCluster(newCluster));
     }
 }
