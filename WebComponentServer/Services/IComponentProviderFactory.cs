65d3072bd96777c02830e939105a80f6fc71a1ea
Danny T <cobreti_@hotmail.com>
2023-11-22 20:23:50 -0500

    rename WebComponentServer to RPWebServer

--- WebComponentServer/Services/IComponentProviderFactory.cs
+++
@@ -1,8 +0,0 @@
-using WebComponentServer.Core;
-
-namespace WebComponentServer.Services;
-
-public interface IComponentProviderFactory
-{
-    IComponentProvider CreateFileProvider(string id, string baseUrl, string sourcePath);
-}
